872baf07de59f501fd81810440f1e5446f9602e3
jiwoo <zbqmgldjfh@gmail.com>
2022-04-11 21:11:48 +0900

    Initial commit

---
+++ src/main/java/shine/restapi/restapi/RestapiApplication.java
@@ -0,0 +1,13 @@
+package shine.restapi.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiApplication.class, args);
+    }
+
+}
