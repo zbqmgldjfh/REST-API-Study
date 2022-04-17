feecbf37f65ee9e52f54987bf347a0820aac1b45
jiwoo <zbqmgldjfh@gmail.com>
2022-04-17 10:26:08 +0900

    test: createEvent_Bad_Request_Global_Error 에서 테스트 코드 오류 부분 수정
    - errors를 직렬화 시킬때 fieldName으로 errors를 추가 해줬기 때문에 error가 errors이름으로 wrapping되어 나온다. 이를 수정

--- src/test/java/shine/restapi/restapi/events/EventControllerTests.java
+++ src/test/java/shine/restapi/restapi/events/EventControllerTests.java
@@ -213,11 +213,11 @@ public class EventControllerTests {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(eventDto)))
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$[0].objectName").exists())
-                .andExpect(jsonPath("$[0].field").doesNotExist())
-                .andExpect(jsonPath("$[0].code").exists())
-                .andExpect(jsonPath("$[0].defaultMessage").exists())
-                .andExpect(jsonPath("$[0].rejectValue").doesNotExist())
+                .andExpect(jsonPath("errors[0].objectName").exists())
+                .andExpect(jsonPath("errors[0].field").doesNotExist())
+                .andExpect(jsonPath("errors[0].code").exists())
+                .andExpect(jsonPath("errors[0].defaultMessage").exists())
+                .andExpect(jsonPath("errors[0].rejectValue").doesNotExist())
         ;
     }
 
