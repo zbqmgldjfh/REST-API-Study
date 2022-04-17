876e55cd7b2a9b40954fc594ff6da05fa869db0b
jiwoo <zbqmgldjfh@gmail.com>
2022-04-17 21:12:12 +0900

    refactor(BaseControllerTest): 모든 테스트들이 상속하는 BaseControllerTest

--- src/test/java/shine/restapi/restapi/events/EventControllerTests.java
+++ src/test/java/shine/restapi/restapi/events/EventControllerTests.java
@@ -1,21 +1,13 @@
 package shine.restapi.restapi.events;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import shine.restapi.restapi.common.RestDocsConfiguration;
+import shine.restapi.restapi.common.BaseControllerTest;
 
 import java.time.LocalDateTime;
 import java.util.stream.IntStream;
@@ -29,24 +21,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-public class EventControllerTests {
 
-    @Autowired
-    MockMvc mockMvc;
-
-    @Autowired
-    ObjectMapper objectMapper;
+public class EventControllerTests extends BaseControllerTest {
 
     @Autowired
     EventRepository eventRepository;
-
-    @Autowired
-    ModelMapper modelMapper;
 
     @Test
     @DisplayName("정상적으로 이벤트 생성하는 테스트")
--- src/test/java/shine/restapi/restapi/index/indexControllerTest.java
+++ src/test/java/shine/restapi/restapi/index/indexControllerTest.java
@@ -1,28 +1,13 @@
 package shine.restapi.restapi.index;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import shine.restapi.restapi.common.RestDocsConfiguration;
+import shine.restapi.restapi.common.BaseControllerTest;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-public class indexControllerTest {
-
-    @Autowired
-    MockMvc mockMvc;
+public class indexControllerTest extends BaseControllerTest {
 
     @Test
     public void indexTest() throws Exception {
